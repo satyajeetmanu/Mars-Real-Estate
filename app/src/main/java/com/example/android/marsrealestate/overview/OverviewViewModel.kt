7acfd4788211cfecca176c315870816a597526f1
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2022-05-31 11:55:14 +0530

    displayed an image on the screen using glide

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -26,9 +26,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -36,11 +33,16 @@ import retrofit2.Response
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status String
     val response: LiveData<String>
-        get() = _response
+        get() = _status
+
+    private val _property = MutableLiveData<MarsProperty>()
+
+    val property: LiveData<MarsProperty>
+        get() = _property
 
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
@@ -64,9 +66,11 @@ class OverviewViewModel : ViewModel() {
 
             try {
                 var listResult = getPropertiesDeferred.await()
-                _response.value = "Size : ${listResult?.size}"
+                if(listResult.isNotEmpty()){
+                    _property.value = listResult[0]
+                }
             } catch (t: Throwable) {
-                _response.value = "Error message: " + t.message
+                _status.value = "Error message: " + t.message
             }
         }
     }
